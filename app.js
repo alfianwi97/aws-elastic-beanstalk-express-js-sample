357c97438456e7d9bf6bdb599bd4566e1f59a150
Alfian Wijayakusuma <alfian.wijaya97@gmail.com>
2021-08-01 18:32:57 +0700

    Add new router
    num1 num2

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/:name', (req, res) => res.send('Hello World '+req.params.name));
+app.get('/:num1/:num2', (req, res) => res.send('Hello World '+(req.params.num1 + req.params.num2)));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
